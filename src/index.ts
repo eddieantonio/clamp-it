2a65508b44df632500896d347b3dfc182e726e5f
Eddie Antonio Santos <eddieantonio@hey.com>
2021-01-30 17:07:30 -0700

    refactor: Extract Class: ClosedClamper

--- src/index.ts
+++ src/index.ts
@@ -3,18 +3,31 @@ interface Clamper {
   atMost(value: number): Clamper;
 }
 
+class ClosedClamper implements Clamper {
+  private _min: number;
+  private _max: number;
+
+  constructor(minimum: number, maximum: number) {
+    this._min = minimum;
+    this._max = maximum;
+  }
+
+  clamp(value: number): number {
+    return Math.min(this._max, Math.max(this._min, value));
+  }
+
+  atMost(): Clamper {
+    return notImplemeted();
+  }
+}
+
 export function atLeast(minimum: number): Clamper {
   return {
     clamp(value: number) {
       return Math.max(minimum, value);
     },
     atMost(maximum): Clamper {
-      return {
-        clamp(value: number) {
-          return Math.min(maximum, Math.max(minimum, value));
-        },
-        atMost: notImplemeted,
-      };
+      return new ClosedClamper(minimum, maximum);
     },
   };
 }
