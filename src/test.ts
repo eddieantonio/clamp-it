f54dc888b8fd94a4bd585875665d7e30fd652374
Eddie Antonio Santos <eddieantonio@hey.com>
2021-01-30 17:21:16 -0700

    test: narrow atLeast

--- src/test.ts
+++ src/test.ts
@@ -16,3 +16,9 @@ test("combine the two", (t) => {
   t.is(atLeast(3).atMost(7).clamp(0), 3);
   t.is(atMost(7).atLeast(3).clamp(0), 3);
 });
+
+test("narrow the range", (t) => {
+  t.is(atLeast(3).atLeast(7).clamp(0), 7);
+  t.is(atLeast(7).atLeast(3).clamp(0), 7);
+  t.is(atLeast(3).atLeast(7).clamp(10), 10);
+});
