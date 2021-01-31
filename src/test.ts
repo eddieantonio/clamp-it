41396f6edb0818c83e086a17bb1f8a4ee241f3be
Eddie Antonio Santos <eddieantonio@hey.com>
2021-01-30 17:24:09 -0700

    test: atMost() gets narrower

--- src/test.ts
+++ src/test.ts
@@ -17,8 +17,22 @@ test("combine the two", (t) => {
   t.is(atMost(7).atLeast(3).clamp(0), 3);
 });
 
-test("narrow the range", (t) => {
+test("narrow atLeast", (t) => {
   t.is(atLeast(3).atLeast(7).clamp(0), 7);
-  t.is(atLeast(7).atLeast(3).clamp(0), 7);
+  t.is(atLeast(7).atLeast(3).clamp(0), 7, "the bounds should not widen");
   t.is(atLeast(3).atLeast(7).clamp(10), 10);
+});
+
+test("narrow atMost", (t) => {
+  t.is(
+    atMost(7).atMost(3).clamp(10),
+    3,
+    "the range should have gotten smaller"
+  );
+  t.is(
+    atMost(3).atMost(7).clamp(10),
+    3,
+    "the range should have stayed the same"
+  );
+  t.is(atMost(3).atMost(7).clamp(0), 0);
 });
